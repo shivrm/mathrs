4242d02a35665477548bdbbea14bfe89cea5c6d8
shivrm <shivrm@proton.me>
2022-04-16 10:25:54 +0530

    Add boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use std::io;
+
+struct Token {}
+
+fn lex(source: &str) -> Vec<Token> {
+    unimplemented!();
+}
+
+fn shunt(tokens: &Vec<Token>) -> Vec<Token> {
+    unimplemented!();
+}
+
+fn eval(tokens: &Vec<Token>) -> f64 {
+    unimplemented!();
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let tokens = lex(&input);
+    let shunted = shunt(&tokens);
+    let value = eval(&shunted);
+
+    println!("Value of expression: {}", value);
+}
