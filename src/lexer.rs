3a64d6bcb5ea9cd9f9770c99169e3d7f43fcaa08
shivrm <shivrm@proton.me>
2022-04-24 22:37:14 +0530

    Implement next_token for lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,21 +1,21 @@
-use std::iter::Peekable
+use std::iter::Peekable;
 
-pub enum Operators {
+pub enum Ops {
     Add,
     Sub,
     Mul,
     Div
 }
 
-pub enum Tokens {
-    Number
-    Identifier
+pub enum Tokens<'a> {
+    Number(i32),
+    Identifier(&'a str),
 
-    OpenParen
-    CloseParen
+    OpenParen,
+    CloseParen,
 
-    UnaryOp(Operator)
-    BinaryOp(Operator)
+    UnaryOp(Ops),
+    BinaryOp(Ops)
 }
 
 pub struct Lexer<'a> {
@@ -43,7 +43,33 @@ impl<'a> Lexer<'a> {
         return next_char;
     }
 
-    pub fn next_token(&mut self) -> Token {
-        todo!();
+    [#inline]
+    fn current_char(&mut self) -> char {
+        self.iter.peek()
+    }
+
+    pub fn next_token(&mut self) -> Tokens<'a> {
+        if let Some(c) = self.current_char() {
+            match c => {
+                '0'..='9' => {
+                    let number = self.read_number();
+                    Tokens::Number(number) 
+                }
+                'A'..='Z' | 'a'..='z' => {
+                    let identifier = self.read_identifier();
+                    Tokens::Identifier(identifier)
+                }
+
+                '(' => Tokens::OpenParen
+                ')' => Tokens::CloseParen
+
+                '+' => Tokens::BinaryOp(Ops::Add)
+                '-' => Tokens::BinaryOp(Ops::Sub)
+                '*' => Tokens::BinaryOp(Ops::Mul)
+                '/' => Tokens::BinaryOp(Ops::Div)
+            
+                _ => panic!("Unhandled char")
+            }
+        }
     }
 }
