e910c1a0a004aa7d7cdf57908399d0f33c8d61e2
shivrm <shivrm@proton.me>
2022-04-24 22:08:11 +0530

    Add lexer constructor and advance method

--- src/lexer.rs
+++ src/lexer.rs
@@ -23,3 +23,27 @@ pub struct Lexer<'a> {
     text: &'a str,
     iter: Peekable<str::Chars<'a>>
 }
+
+impl<'a> Lexer<'a> {
+    pub fn new(text: &'a str) -> Self {
+        return Lexer {
+            text,
+            pos: 0,
+            iter: text.chars().peekable()
+        }
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        let next_char = self.iter.next();
+
+        if let Some(_) = next_char {
+            self.pos += 1;
+        }
+
+        return next_char;
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        todo!();
+    }
+}
