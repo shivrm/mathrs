66728d356dc91b716d846879e55537cc21c30122
shivrm <shivrm@proton.me>
2022-04-25 11:41:02 +0530

    Implement parser `new` and `expect`

--- src/mathrs/parser.rs
+++ src/mathrs/parser.rs
@@ -1,14 +1,18 @@
-use mathrs::lexer::*;
+use crate::mathrs::lexer::*;
+
+fn variant_eq<T>(a: &T, b: &T) -> bool {
+    std::mem::discriminant(a) == std::mem::discriminant(b)
+}
 
 enum AstNode {
     BinOp {
-        left: AstNode,
+        left: Box<AstNode>,
         op: Ops,
-        right: AstNode
+        right: Box<AstNode>
     },
     UnOp {
         op: Ops,
-        operand: AstNode
+        operand: Box<AstNode>
     },
     Number(i32)
 }
@@ -18,3 +22,19 @@ struct Parser<'a> {
     current_token: Token
 }
 
+impl<'a> Parser<'a> {
+    pub fn new(text: &'a str) -> Self {
+        let mut lexer = Lexer::new(text);
+        Parser {
+            current_token: lexer.next_token().unwrap(),
+            lexer
+        }
+    }
+
+    fn expect(&mut self, token_type: Token) {
+        if !variant_eq(&self.current_token, &token_type) {
+            panic!("Expected {token_type:?}");
+        }
+        self.current_token = self.lexer.next_token().unwrap();
+    }
+}
