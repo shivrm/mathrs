89f5254afb5d7205003cd902af9377f1c9a2767b
shivrm <shivrm@proton.me>
2022-04-27 18:09:41 +0530

    Re-add multi-line input support

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,27 @@ use std::io::prelude::*;
 mod mathrs;
 
 fn main() {
-    print!("Enter an expression: ");
+    print!(concat!(
+        "Welcome to mathrs, a math expression parser!\n",
+        "Please enter your math expression.\n",
+        "You can use multiple line to enter your expression.\n",
+        "Entering a blank line will start evaluation.\n\n"
+    ));
     io::stdout().flush().expect("Could not flush buffer");
 
     let mut text = String::new();
-    io::stdin()
-        .read_line(&mut text)
-        .expect("Failed to read line");
+    
+    loop {
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+
+        if line.trim().is_empty() {
+            break;
+        }
+        text = text + &line;
+    }
 
     let result = mathrs::eval(&mut text).unwrap();
     println!("Value: {result}");
